94cfb01114eea74c9662870617058d1bd4f0a678
Jacob Hands <jrhands@gmail.com>
2018-07-24 13:14:45 -0400

    Initial commit, started creating class data

---
+++ Lab8/Lab8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Welcome to our C# class. Which student would you like to learn more about? (enter a number 1-20): ");
+            String[][] classmates = new String[20][];
+            String[] fabricia = { "Fabricia Gensch" };
+            String[] cintia = { "Cintia Buachalla" };
+            String[] elvira = { "Elvira Auteberry" };
+            String[] graciana = { "Graciana Koning" };
+            String[] gaetan = { "Gaetan Dickinson" };
+            String[] cian = { "Fabricia Gensch" };
+            String[] gerolt = { "Fabricia Gensch" };
+            String[] perlita = { "Fabricia Gensch" };
+            String[] elise = { "Fabricia Gensch" };
+            String[] trina = { "Fabricia Gensch" };
+            String[] malini = { "Fabricia Gensch" };
+            String[] anna = { "Fabricia Gensch" };
+            String[] aina = { "Fabricia Gensch" };
+            String[] fae = { "Fabricia Gensch" };
+            String[] edita = { "Fabricia Gensch" };
+            String[] thyge = { "Fabricia Gensch" };
+            String[] kapono = { "Fabricia Gensch" };
+            String[] kiley = { "Fabricia Gensch" };
+            String[] lila = { "Fabricia Gensch" };
+            String[] clair = { "Fabricia Gensch" };
+
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int index))
+                {
+
+                }
+            }
+
+        }
+    }
+}
